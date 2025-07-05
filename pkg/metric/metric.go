fffd385463097d82571f5d065b26607a7314caa2
Tomasz Nowodziński <tomasz.nowodzinski@sadsharkdev.pl>
2025-07-05 12:39:13 +0200

    feat: CU-8699hu9hk - Health Checks and Basic Metrics

--- pkg/metric/metric.go
+++ pkg/metric/metric.go
@@ -12,30 +12,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-//go:generate mockgen -destination=../../mocks/metrics_mock.go -package=mocks github.com/kubecano/cano-collector/pkg/metric MetricsInterface
-type MetricsInterface interface {
-	PrometheusMiddleware() gin.HandlerFunc
-	ObserveAlert(receiver string, status string)
-	ClearMetrics()
-
-	// Alert processing metrics
-	IncAlertsProcessed(alertName, severity, source string)
-	ObserveAlertProcessingDuration(alertName string, workflowCount int, duration time.Duration)
-	IncAlertErrors(alertName, errorType string)
-
-	// Destination metrics
-	IncDestinationMessagesSent(destinationName, destinationType, status string)
-	ObserveDestinationSendDuration(destinationName, destinationType string, duration time.Duration)
-	IncDestinationErrors(destinationName, destinationType, errorType string)
-
-	// HTTP metrics
-	ObserveHTTPRequestDuration(method, path, status string, duration time.Duration)
-
-	// Routing metrics
-	IncRoutingDecisions(teamName, destinationType, decision string)
-	IncTeamsMatched(teamName, alertName string)
-}
-
 type MetricsCollector struct {
 	httpRequestsTotal            *prometheus.CounterVec
 	httpRequestDuration          *prometheus.HistogramVec
